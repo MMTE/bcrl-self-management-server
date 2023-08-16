34429941ac588f3e7700e6aab94c3cfdd39a1ff5
MMTE <hi.taleghani@gmail.com>
2023-08-17 00:23:34 +0330

    fix user role was not changing

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,8 @@ class User extends Authenticatable implements FilamentUser
         'password',
         'status',
         'phone',
-        'age'
+        'age',
+        'role'
     ];
 
     /**
